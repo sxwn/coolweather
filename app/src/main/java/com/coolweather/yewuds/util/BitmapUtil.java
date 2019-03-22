9399f625fbc15c0e4638854514f520bc91993819
wp <wdzaxiaowei@126.com>
2019-03-22 20:50:50 +0800

    添加bitmap工具类

--- app/src/main/java/com/coolweather/yewuds/util/BitmapUtil.java
+++ app/src/main/java/com/coolweather/yewuds/util/BitmapUtil.java
@@ -1,9 +1,40 @@
 package com.coolweather.yewuds.util;
 
+import android.media.ExifInterface;
+
+import java.io.IOException;
+
 /**
  * 图片压缩
  * Created by weip on 2019/3/22.
  */
 
 public class BitmapUtil {
+
+    /**
+     * 获取图片的旋转角度
+     * @param filePath
+     * @return
+     */
+    public static int getRotateAngle(String filePath) {
+        int rotate_angle = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(filePath);
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate_angle = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate_angle = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate_angle = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rotate_angle;
+    }
 }
