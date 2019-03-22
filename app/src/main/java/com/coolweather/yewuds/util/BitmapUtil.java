340e029fe2656051551d7938d9ab735e576a86ca
wp <wdzaxiaowei@126.com>
2019-03-22 21:01:12 +0800

    添加bitmap工具类

--- app/src/main/java/com/coolweather/yewuds/util/BitmapUtil.java
+++ app/src/main/java/com/coolweather/yewuds/util/BitmapUtil.java
@@ -1,7 +1,16 @@
 package com.coolweather.yewuds.util;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.media.ExifInterface;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -37,5 +46,95 @@ public class BitmapUtil {
             e.printStackTrace();
         }
         return rotate_angle;
+    }
+
+    /**
+     * 旋转图片角度
+     * @param angle
+     * @param bitmap
+     * @return
+     */
+    public static Bitmap setRotateAngle(int angle, Bitmap bitmap) {
+
+        if (bitmap != null) {
+            Matrix m = new Matrix();
+            m.postRotate(angle);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                    bitmap.getHeight(), m, true);
+            return bitmap;
+        }
+        return bitmap;
+
+    }
+
+    //转换为圆形状的bitmap
+    public static Bitmap createCircleImage(Bitmap source) {
+        int length = source.getWidth() < source.getHeight() ? source.getWidth() : source.getHeight();
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        Bitmap target = Bitmap.createBitmap(length, length, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(target);
+        canvas.drawCircle(length / 2, length / 2, length / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(source, 0, 0, paint);
+        return target;
+    }
+    /**
+     * 图片压缩-质量压缩
+     * @param filePath 源图片路径
+     * @return 压缩后的路径
+     */
+    public static String compressImage(String filePath) {
+        //原文件
+        File oldFile = new File(filePath);
+        //压缩文件路径 照片路径/
+        String targetPath = oldFile.getPath();
+        int quality = 50;//压缩比例0-100
+        Bitmap bm = getSmallBitmap(filePath);//获取一定尺寸的图片
+        int degree = getRotateAngle(filePath);//获取相片拍摄角度
+
+        if (degree != 0) {//旋转照片角度，防止头像横着显示
+            bm = setRotateAngle(degree,bm);
+        }
+        File outputFile = new File(targetPath);
+        try {
+            if (!outputFile.exists()) {
+                outputFile.getParentFile().mkdirs();
+                //outputFile.createNewFile();
+            } else {
+                outputFile.delete();
+            }
+            FileOutputStream out = new FileOutputStream(outputFile);
+            bm.compress(Bitmap.CompressFormat.JPEG, quality, out);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return filePath;
+        }
+        return outputFile.getPath();
+    }
+
+    public static Bitmap getSmallBitmap(String filePath) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;//只解析图片边沿，获取宽高
+        BitmapFactory.decodeFile(filePath, options);
+        // 计算缩放比
+        options.inSampleSize = calculateInSampleSize(options, 480, 800);
+        // 完整解析图片返回bitmap
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options,
+                                            int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        return inSampleSize;
     }
 }
