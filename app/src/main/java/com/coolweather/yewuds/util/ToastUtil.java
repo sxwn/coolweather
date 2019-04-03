bc43efd4059979ca9980c53ad44db7ffef57a04a
wp <wdzaxiaowei@126.com>
2019-04-03 21:04:29 +0800

    添加bitmap工具类

--- app/src/main/java/com/coolweather/yewuds/util/ToastUtil.java
+++ app/src/main/java/com/coolweather/yewuds/util/ToastUtil.java
@@ -1,9 +1,15 @@
 package com.coolweather.yewuds.util;
 
+import android.widget.Toast;
+
 /**
+ * 土司
  * Created by weip on 2019/3/14.
  */
 
 public class ToastUtil {
 
+    public static ToastUtil getInstance(){
+        return new ToastUtil();
+    }
 }
